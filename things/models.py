90498158e3ffed6f911f0b7140c9db42999ec517
Caleb Chan <caleb.chan@kcl.ac.uk>
2023-10-18 19:31:17 +0100

    Add quantity restriction validator to user model.

--- things/models.py
+++ things/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import Model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Thing(Model):
@@ -15,4 +16,8 @@ class Thing(Model):
     )
     quantity = models.IntegerField(
         unique=False,
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        ]
     )
